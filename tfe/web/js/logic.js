3132a982633095539c2acc24f03e40d880abc6ae
jpges <jpastor@evitrum.com>
2020-03-05 01:23:02 +0100

    Consola estudiantes

---
+++ tfe/web/js/logic.js
@@ -0,0 +1,25 @@
+// Acciones del empleado -----------------------------------------------------
+function loginEstudiante(){
+    location.replace("estudiantes.html");
+    var address = document.getElementById("inputAccount").value;
+  	var pss = document.getElementById("inputPassword").value;
+  	try{
+  		web3.personal.unlockAccount(address, pss, 0);
+  		var exist = plataforma.existeEmpleado.call(address, {from: accounts[0], gas:30000});
+  			if (exist){
+  	    		localStorage.setItem("inputAccount", address);
+  				location.replace("estudiantes.html");
+  			}else{
+  				alert("La cuenta de estudiante indicada no existe en el sistema");
+  			}
+  	}catch(error) {
+  		alert("¡Contraseña incorrecta!");
+  	}
+  }
+
+
+
+function encadenar(){
+    var consola = document.getElementById('consola').innerHTML;
+    document.getElementById('consola').innerHTML = consola.concat('ashkjdjl asdsad lkjlkasjd laksjda sdlkajsdlk jasldkja slkdjas dlkasjd laksdj');
+}
